10fa531d74a16f9f9974c658a596b9523fb0a2cd
dhrutik <31826579+dhrutik@users.noreply.github.com>
2019-09-03 20:42:59 -0700

    Xcode Initial Project (this is just to test functionality)

---
+++ ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  qPacer
+//
+//  Created by Dhruti Kuchibhotla on 8/5/19.
+//  Copyright © 2019 Dhruti Kuchibhotla. All rights reserved.
+//
+
+import UIKit
+
+var arraySize = 0;
+class ViewController: UIViewController {
+
+    @IBOutlet weak var field: UITextField!
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    
+    }
+    
+    @IBAction func changeText(_ sender: UIButton) {
+        let text1 = field.text;
+        label.text = text1;
+    }
+    
+    
+    
+        
+    
+}
+
+extension UIViewController{
+    
+}
