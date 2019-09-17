ca2dc8b604b7ecbcfd755d30d084488dd05af937
Dhruti <srisaidhruti@gmail.com>
2019-09-17 12:34:46 -0700

    changed initial design of screens

---
+++ qPacer/DataSetterViewController.swift
@@ -0,0 +1,21 @@
+//
+//  dataSetterViewController.swift
+//  qPacer
+//
+//  Created by Dhruti Kuchibhotla on 9/17/19.
+//  Copyright © 2019 Dhruti Kuchibhotla. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DataSetterViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+}
